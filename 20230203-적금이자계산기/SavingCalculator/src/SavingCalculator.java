1c20c2510643ccd7dcaeff0e1a38ee3fe4488506
jackpods <>
2023-02-03 11:35:10 +0900

    20230203-적금계산기-장준영

--- 20230203-적금이자계산기/SavingCalculator/src/SavingCalculator.java
+++ 20230203-적금이자계산기/SavingCalculator/src/SavingCalculator.java
@@ -4,6 +4,7 @@ public class SavingCalculator {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
+
         System.out.println("점금 이자 계산기");
         System.out.println("월적립액을 입력해주세요(원)");
         int save = scanner.nextInt();
@@ -14,13 +15,28 @@ public class SavingCalculator {
 
 
         int sum = save * 12 * years ;
-
-        double interest = save * 0.001 * percent * years ;
-        double sad = interest * 0.846 ;
+        double r = Math.pow((1 + percent),12*years);
+//        double interest =save *(1 +percent) *( r-1/percent) ;
+//                //save * 0.001 * percent * years ;
+//        double sad = interest * 0.846 ;
         String format = String.format("%,d",sum);
+//        System.out.println(interest-sum);
 
+            int sum1 = 0;
+            int total = 0;
+
+        for(int i=12;0<i;i--) {
+            double interest =(save * percent*0.01)*(i/12) ;
+
+            total += interest;
+            sum += total;
+            System.out.println(sum);
+
+        }
+        System.out.println(total);
         System.out.println(String.format("원금합계: %,d 원",sum));
-        System.out.println(String.format("세후이자: %,d 원"+sad));
+        System.out.println("세후이자: 원");
         System.out.println("세후 총 수령액: ");
+
     }
 }
