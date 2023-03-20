ea0647d40b7a8cd3a4a8b37701aae5e0facb5203
jackpods <>
2023-03-20 09:09:10 +0900

    20230320-수박수박수박수

---
+++ 20230320-수박수박수박수박수/Repeat/src/test/java/RepeatTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RepeatTest {
+    @Test
+    void repeat() {
+
+        String answer = "";
+        if ((2n-1) % 2 != 0){
+            '수' 를 출력해서 answer에 더하고
+        }
+        if (n이 짝수이면){
+            '박' 을 출력해서 answer에 더한다.
+        }
+    }
+
+}
