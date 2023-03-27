fa0922416830835c9e64933bb47a7ccb7028316e
jackpods <>
2023-03-27 09:11:03 +0900

    ignore

---
+++ 20230324-SpiralMatrix/SpiralMatrix/src/main/java/SpiralMatrix.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SpiralMatrix {
+    public static void main(String[] args) {
+//        준비
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("배열의 한변 길이: ");
+        int length = scanner.nextInt();
+
+        System.out.println("나선형 배열: ");
+
+//        입력
+
+//        처리
+
+//        출력
+
+    }
+}
