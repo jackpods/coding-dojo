acb944b324723365e14251ba03f480c273c21352
jackpods <>
2023-03-29 09:46:26 +0900

    3.29문자열내림차순으로배치

---
+++ 20230329-문자열 내림차순으로 배치하기/Solution/src/test/java/SolutionTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    @Test
+    void test(){
+        String s = "Zbcdefg";
+
+//       String[] a = s.split("");
+
+        char a = s.charAt(0);
+    }
+}
