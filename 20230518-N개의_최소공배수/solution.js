058675e4cd4db2cac2c4c68bd02584c4e4519d63
jackpods <>
2023-05-19 00:04:44 +0900

    N개의 최소공배수

---
+++ 20230518-N개의_최소공배수/solution.js
@@ -0,0 +1,9 @@
+export default function solution(array) {
+  const answer = 0;
+  if (array.includes(1)) {
+    for (let i = 0; i < array.length; i += 1) {
+      
+    }
+  }
+  return answer;
+}
---
+++ 20230518-N개의_최소공배수/solution.test.js
@@ -0,0 +1,5 @@
+const { default: solution } = require('./solution');
+
+test('solution', () => {
+  expect();
+});
